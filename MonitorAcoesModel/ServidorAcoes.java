d13366c4a202257aff43c251cf8dfdf1ff0d7d63
robsonfm <nosborffm@gmail.com>
2016-05-17 05:07:14 -0300

    Alterações na classe ServidorAcoes
    implementado o registraAcaoCliente
    implementado o comunicaAlteracao

--- MonitorAcoesModel/ServidorAcoes.java
+++ MonitorAcoesModel/ServidorAcoes.java
@@ -8,9 +8,11 @@ public class ServidorAcoes extends UnicastRemoteObject implements IServidorAcoes
 	
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Acao> acoesServidor;
+	private ArrayList<InteresseAcao> interesse;
 	
 	public ServidorAcoes() throws RemoteException {
 		this.acoesServidor = new ArrayList<Acao>();
+		this.interesse = new ArrayList<InteresseAcao>();
 	}
 	
 	public ArrayList<Acao> getAcoesServidor() throws RemoteException {
@@ -25,20 +27,49 @@ public class ServidorAcoes extends UnicastRemoteObject implements IServidorAcoes
 		String nomeAcao = acao.getNomeAcao();
 		int i = 0;
 		boolean achou = false;
-		for (Acao acaoInteracao : acoesServidor){
+		for (Acao acaoInteracao : this.acoesServidor){
 			if (acaoInteracao.getNomeAcao().equals(nomeAcao)){
 				achou = true;
-				i = acoesServidor.indexOf(acaoInteracao);
+				i = this.acoesServidor.indexOf(acaoInteracao);
 			}
 		}
 		if (!achou){
 			Acao novaAcao = new Acao(nomeAcao, novoPreco);
 			this.acoesServidor.add(novaAcao);
-			//InteresseAcao novoInteresse = new InteresseAcao(novaAcao); duvida aki
+			InteresseAcao novoInteresse = new InteresseAcao(novaAcao);
+			this.interesse.add(novoInteresse);
 		}else{
 			HistoricoPrecos preco = new HistoricoPrecos(novoPreco);
 			this.acoesServidor.get(i).getPrecosAcao().add(preco);
-			//comunicaAlteracao();
+			comunicaAlteracao(this.acoesServidor.get(i).getNomeAcao());
+		}	
+	}
+	
+	public void registraAcaoCliente(IClienteInvestidor registraCliente)throws RemoteException {
+		String nomeAcao = registraCliente.getAcao().getNomeAcao();
+		int i = 0;
+		for(InteresseAcao comunica : this.interesse){
+			if (comunica.getAcao().getNomeAcao().equals(nomeAcao)){
+				i = this.interesse.indexOf(comunica);
+			}
+		}
+		this.interesse.get(i).getClientesInteresse().add(registraCliente);
+		
+		
+	}
+
+	public void comunicaAlteracao(String acao) throws RemoteException {
+		int i = 0;
+		int j = 0;
+		for(InteresseAcao comunica : this.interesse){
+			if (comunica.getAcao().getNomeAcao().equals(acao)){
+				i = this.interesse.indexOf(comunica);
+			}
+		}
+		while (j<this.interesse.get(i).getClientesInteresse().size()){
+			//tem q mudar a passagem aki
+			this.interesse.get(i).getClientesInteresse().get(j++).notificaAlteracao("acao alterada");
 		}
 	}
+
 }
