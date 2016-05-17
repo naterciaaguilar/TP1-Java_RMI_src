51fec7048a5ef3475f0a31bdad455de9eaffcef1
Natércia Aguilar Batista <natercia.aguilar@gmail.com>
2016-05-17 01:46:12 -0300

    Main Cliente Administrador

--- MonitorAcoesController/MainClienteAdministradorController.java
+++ MonitorAcoesController/MainClienteAdministradorController.java
@@ -8,23 +8,14 @@ import MonitorAcoesModel.IServidorAcoes;
 
 public class MainClienteAdministradorController {
 	public static void main (String[] args) {
-		boolean continua = true;
 		int porta = Integer.parseInt(args[0]);
-		try{
+		
+		try {
 			IServidorAcoes obj = (IServidorAcoes)Naming.lookup("rmi://localhost:" + porta + "/servidordeacoes");
-			while (continua) {
-				String acao = JOptionPane.showInputDialog("Digite a acao: ");
-				double valor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor: "));
-				if (acao.equals("ok")){
-					continua = false;
-					acao = "tchau";
-					JOptionPane.showMessageDialog(null, acao);
-				}else{
-					obj.setVetorAcao(acao,valor);
-					JOptionPane.showMessageDialog(null,obj.retornaAcao(acao));
-				}
-			}
-		}catch (Exception e){
+			
+			ClienteAdministradorController controlClienteAdministrador = new ClienteAdministradorController();
+			controlClienteAdministrador.inicializaClienteAdministrador();
+		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
